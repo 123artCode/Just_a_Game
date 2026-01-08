121ac5502673eb0db10711656dcb174a0099d16c
123artCode <art-faden0o@icloud.com>
2026-01-08 20:01:47 +0100

    fixed all the remaining bugs with appwrite and set it up for highscores.

--- Appwrite_test.py
+++ Appwrite_test.py
@@ -5,7 +5,7 @@ from appwrite.id import ID
 client = Client()
 client.set_endpoint('https://fra.cloud.appwrite.io/v1')
 client.set_project('just-a-game')
-client.set_key('Python_Game')
+client.set_key('standard_0c4f19e43f31930ef9768e31c3cb460ee33cfdefe4f165209cdd9eabf07da8a6a230ccefdf9c4668f7cf73800d333cc02ac76ce1550dc9ae8fd2c80357a594223745160eb87107d5bd592fabd1c57c1952231135f7f1acc27d2c0bd9a93c0befba6f007272e50f880238b51d73b77550ac15ef5c2fc2edf1186c30e8420c7634')
 
 
 tablesDB = TablesDB(client)
@@ -17,40 +17,43 @@ def prepare_database():
   global todoDatabase
   global todoTable
 
-  todoDatabase = tablesDB.create(
-    database_id=ID.unique(),
-    name='TodosDB'
-  )
+  # todoDatabase = tablesDB.create(
+  #   database_id=ID.unique(),
+  #   name='TodosDB'
+  # )
+  todoDatabase = tablesDB.get(database_id='695ff9080035072e95c4')
 
-  todoTable = tablesDB.create_table(
-    database_id=todoDatabase['$id'],
-    table_id=ID.unique(),
-    name='Todos'
-  )
+  # todoTable = tablesDB.create_table(
+  #   database_id=todoDatabase['$id'],
+  #   table_id=ID.unique(),
+  #   name='Todos'
+  # )
 
-  tablesDB.create_string_column(
-    database_id=todoDatabase['$id'],
-    table_id=todoTable['$id'],
-    key='title',
-    size=255,
-    required=True
-  )
+  todoTable = tablesDB.get_table(database_id='695ff9080035072e95c4', table_id='scores')
 
-  tablesDB.create_string_column(
-    database_id=todoDatabase['$id'],
-    table_id=todoTable['$id'],
-    key='description',
-    size=255,
-    required=False,
-    default='This is a test description.'
-  )
+  # tablesDB.create_string_column(
+  #   database_id=todoDatabase['$id'],
+  #   table_id=todoTable['$id'],
+  #   key='title',
+  #   size=255,
+  #   required=True
+  # )
 
-  tablesDB.create_boolean_column(
-    database_id=todoDatabase['$id'],
-    table_id=todoTable['$id'],
-    key='isComplete',
-    required=True
-  )
+  # tablesDB.create_string_column(
+  #   database_id=todoDatabase['$id'],
+  #   table_id=todoTable['$id'],
+  #   key='description',
+  #   size=255,
+  #   required=False,
+  #   default='This is a test description.'
+  # )
+
+  # tablesDB.create_boolean_column(
+  #   database_id=todoDatabase['$id'],
+  #   table_id=todoTable['$id'],
+  #   key='isComplete',
+  #   required=True
+  # )
 
 
 
@@ -111,7 +114,7 @@ def get_todos():
   )
   print("Todos:")
   for todo in todos['rows']:
-    print(f"Title: {todo['title']}\nDescription: {todo['description']}\nIs Todo Complete: {todo['isComplete']}\n\n")
+    print(f"Name: {todo['Name']}\nScore: {todo['Score']}\n\n")
 
 def get_completed_todos():
   # Use queries to filter completed todos with pagination
@@ -126,7 +129,7 @@ def get_completed_todos():
   )
   print("Completed todos (limited to 5):")
   for todo in todos['rows']:
-    print(f"Title: {todo['title']}\nDescription: {todo['description']}\nIs Todo Complete: {todo['isComplete']}\n\n")
+    print(f"\nDescription: {todo['description']}\nIs Todo Complete: {todo['isComplete']}\n\n")
 
 def get_incomplete_todos():
   # Query for incomplete todos
@@ -144,10 +147,10 @@ def get_incomplete_todos():
 
 if __name__ == "__main__":
   prepare_database()
-  seed_database()
+  # seed_database()
   get_todos()
-  get_completed_todos()
-  get_incomplete_todos()
+  # get_completed_todos()
+  # get_incomplete_todos()
 
 
 
